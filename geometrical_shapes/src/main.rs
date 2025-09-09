c67a893c9a794fdb36490592b750314df689426b
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-09 10:11:38 +0100

    file initialising

---
+++ geometrical_shapes/src/main.rs
@@ -0,0 +1,37 @@
+mod geometrical_shapes;
+
+use geometrical_shapes as gs;
+use gs::{Displayable, Drawable};
+use raster::{Color, Image};
+
+fn main() {
+    let mut image = Image::blank(1000, 1000);
+
+    gs::Line::random(image.width, image.height).draw(&mut image);
+
+    gs::Point::random(image.width, image.height).draw(&mut image);
+
+    let rectangle = gs::Rectangle::new(&gs::Point::new(150, 300), &gs::Point::new(50, 60));
+    rectangle.draw(&mut image);
+
+    let triangle = gs::Triangle::new(
+        &gs::Point::new(500, 500),
+        &gs::Point::new(250, 700),
+        &gs::Point::new(700, 800),
+    );
+    triangle.draw(&mut image);
+
+    for _ in 1..50 {
+        gs::Circle::random(image.width, image.height).draw(&mut image);
+    }
+
+    raster::save(&image, "image.png").unwrap();
+}
+
+impl Displayable for Image {
+    fn display(&mut self, x: i32, y: i32, color: Color) {
+        if x >= 0 && x < self.width && y >= 0 && y < self.height {
+            self.set_pixel(x, y, color).unwrap();
+        }
+    }
+}
